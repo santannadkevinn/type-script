02e8d40bedb7b2b60ff34c17a8c7ee078b702a91
santannadkevinn <67100140+santannadkevinn@users.noreply.github.com>
2023-05-02 23:35:31 -0300

    primeira aula

---
+++ Aula-1/index.js
@@ -0,0 +1,10 @@
+const firstName = 'Kevin'
+const anotherName = 1
+const x = true
+
+function greeting(name: string){
+    console.log("Ola " + name);
+}
+greeting(firstName)
+greeting(anotherName)
+greeting(x)
